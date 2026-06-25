c0f70b7d4cd43dec03fa6a6d610fb2b4d5b73e0d
Mete Özsoy <emrullahmeteozsoy9@gmail.com>
2026-06-25 14:02:20 +0300

    chore: support POSTGRES_URL from Vercel natively

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg'
 import * as schema from './schema'
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
 })
 
 export const db = drizzle(pool, { schema })
